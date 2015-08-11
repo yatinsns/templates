f73437c83db604260592c38b33e750e713279f3d
Yatin Sarbalia <yatinsns@gmail.com>
2015-08-12 01:24:06 +0530

    Add optparse example file

---
+++ ruby/example.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# require 'optparse'
+
+def main
+  # opts = {}
+  # OptionParser.new do |o|
+  #   o.on('-a', '--aDesc A', Integer) { |x| opts[:a] = x }
+  #   o.on('-b', '--bDesc B', Integer) { |x| opts[:b] = x }
+  #   o.on('--c') { opts[:c] = :C }
+  # end.parse!
+  # Do something with 'opts'
+end
+
+main if __FILE__ == $0
